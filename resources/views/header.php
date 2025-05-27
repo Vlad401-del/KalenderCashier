129aafcba9ef5e908d20ed21ab1cda7530726060
Vlad401-del <vladfromsoara@student.ub.ac.id>
2025-05-27 08:23:23 +0700

    Add files via upload

---
+++ resources/views/header.php
@@ -0,0 +1,33 @@
+<header class="bg-success text-white shadow-sm">
+  <nav class="navbar navbar-expand-md navbar-dark container py-3">
+    <a class="navbar-brand fw-bold" href="#">💰 Cashie Dashboard</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Calendar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Treasury</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Transaction</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Report</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Archive</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="?c=UserController&m=logoutProcess">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
